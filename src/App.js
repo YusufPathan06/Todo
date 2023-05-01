c35afdbcd29cc4f040415d248454557ac6c0732f
Yusuf Pathan <yusuf.pathan@edstem.com>
2023-05-01 23:31:42 +0530

    created basic todo application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, { text: inputValue.trim() }]);
+      setInputValue('');
+    }
+  };
+
+  const handleEditTodo = (index, newText) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = { text: newText };
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="text"
+              value={todo.text}
+              onChange={(e) => handleEditTodo(index, e.target.value)}
+            />
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
